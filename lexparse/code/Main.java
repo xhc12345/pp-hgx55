409a707b7bd00134ecd96ee27df20d10ba029a49
xhc12345 <hcxu@utexas.edu>
2023-09-06 19:53:37 -0500

    Main.java running

---
+++ lexparse/code/Main.java
@@ -0,0 +1,29 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.nio.file.Path;
+import java.nio.file.FileSystems;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n\n########################\nRunning semantic analyzer...\n########################");
+        System.out.println("Input: "+Arrays.toString(args));
+        if(args.length!=1){
+            System.out.println("No files to test");
+            return;
+        }
+        String inputPath = args[0];
+        Path path = FileSystems.getDefault().getPath(inputPath);
+        CharStream input = CharStreams.fromPath(path);
+        System.out.println("The input is:"+input);
+        SimpleLangLexer lexer = new SimpleLangLexer(input);
+        SimpleLangParser parser = new SimpleLangParser(new CommonTokenStream(lexer));
+        ParseTree tree = parser.project();
+
+        Visitor visitor = new Visitor();
+        visitor.visit(tree);
+    }
+}
