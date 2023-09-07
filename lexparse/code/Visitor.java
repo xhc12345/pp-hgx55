44e0da53d2d48b9eaa5312246b0435e18dc29dd8
xhc12345 <hcxu@utexas.edu>
2023-09-06 23:45:11 -0500

    entry error

--- lexparse/code/Visitor.java
+++ lexparse/code/Visitor.java
@@ -1,20 +1,41 @@
 import java.util.List;
 
 public class Visitor extends SimpleLangBaseVisitor<Integer> {
+
+
     @Override public Integer visitProject(SimpleLangParser.ProjectContext ctx){
         String projectName = ctx.ID().getText();
         System.out.println("project name is "+projectName);
+
+        boolean hasEntry=false, voidEntry=false, noParamEntry=false;
         List<SimpleLangParser.MethodDeclContext> allMethods = ctx.methodDecl();
-        System.out.print(allMethods.size()+" methods: ");
         for(SimpleLangParser.MethodDeclContext m: allMethods){
-            System.out.print(m.ID().getText()+',');
-        }System.out.println();
+            // search for entry() and check if it's good
+            if(m.ID().getText().equals("entry")){
+                hasEntry = true;
+                if(m.type()==null){
+                    voidEntry=true;
+                }
+                if(m.formPars()==null){
+                    noParamEntry=true;
+                }
+            }
+        }
+        if(!hasEntry || !voidEntry || !noParamEntry){
+            System.out.println("ENTRY ERROR");
+        }
+
         return visitChildren(ctx);
     }
 
     @Override public Integer visitClassDecl(SimpleLangParser.ClassDeclContext ctx) {
         String className = ctx.ID().getText();
         System.out.println("class is "+className);
+        return visitChildren(ctx);
+    }
+
+    @Override public Integer visitMethodDecl(SimpleLangParser.MethodDeclContext ctx) {
+        
         return visitChildren(ctx);
     }
 }
