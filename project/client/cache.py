809ba46e6155e2f73793e9ec86ddedb3aba46cb5
Haochen Xu <hcxu@utexas.edu>
2023-10-24 04:15:51 -0500

    fetch row and insert row for client cache

--- project/client/cache.py
+++ project/client/cache.py
@@ -5,10 +5,31 @@ import sqlite3
 
 MODIFYING_KEYWORDS = ["CREATE", "MERGE", "SET", "DELETE"]
 
+DEFAULT_DB_NAME = 'clientCache.db'
+DEFAULT_TABLE_NAME = "cache_table"
+
 class cacheDB:
-    def __init__(self) -> None:
+    def __init__(self, dbName: str = DEFAULT_DB_NAME, tableName: str = DEFAULT_TABLE_NAME) -> None:
+        self.dbName:str = 'cache/'+dbName
+        self.tableName:str = tableName
         print("Initializing client cache database (SQLite)")
-        # TODO: initialize sqlite db here
+        self.connection = sqlite3.connect(self.dbName)
+        cursor = self.connection.cursor()
+        # Create the table with the keys needed to represent query & response
+        cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {self.tableName} (
+                query TEXT PRIMARY KEY,
+                code INTEGER,
+                success BOOLEAN,
+                message TEXT,
+                clearCache BOOLEAN
+            )
+        ''')
+        self.connection.commit() # reflect changes
+
+    def __del__(self):
+        print("Closing connection to client cache database (SQLite)")
+        self.connection.close()
 
     def clear_all_cache(self) -> bool:
         wiped: bool = False
@@ -49,6 +70,50 @@ class cacheDB:
         operationCompleted: bool = False
         # TODO: put/update target query and its response in the SQL table
         return operationCompleted
+    
+    def fetch_row(self, query:str) -> responseObj:
+        cursor = self.connection.cursor()
+        # Execute a SELECT statement to fetch the row
+        cursor.execute(f'SELECT * FROM {self.tabelName} WHERE query = ?', (query,))
+        row = cursor.fetchone()
+
+        if row:
+            # Extract the values for the keys
+            query, code, success, message, clearCache = row
+            response = responseObj(code, {"success":success, "message":message, "clearCache":clearCache})
+            return response
+        else:
+            print("Query not found in cache:", query)
+            return None
+        
+    def insert_row(self, query:str, response:responseObj):
+        cursor = self.connection.cursor()
+
+        # Check if the query exists
+        cursor.execute(f'SELECT * FROM {self.tableName} WHERE query = ?', (query,))
+        existing_row = cursor.fetchone()
+
+        if existing_row:
+            # Row with the query exists, update it
+            cursor.execute(
+                f'''
+                    UPDATE {self.tableName}
+                    SET code = ?, success = ?, message = ?, clearCache = ?
+                    WHERE query = ?
+                ''',
+                (*response.toTuple(), query)
+            )
+        else:
+            # Row with the query doesn't exist, insert a new row
+            cursor.execute(
+                f'''
+                    INSERT INTO {self.tableName} (query, code, success, message, clearCache)
+                    VALUES (?, ?, ?, ?)
+                ''',
+                (query, *response.toTuple())
+            )
+
+        self.connection.commit()
     
     def __is_cacheable(query: str, response: responseObj) -> bool:
         """Checks if query contains commands that modifies the graph. If true then query non-cacheable.
--- project/client/response.py
+++ project/client/response.py
@@ -4,3 +4,10 @@ class responseObj:
         self.success:bool = responseBody.get('success', False)
         self.message:str = responseBody.get('message', "Failed to create response object")
         self.clearCache:bool = responseBody.get('clearCache', False)
+
+    def __str__(self) -> str:
+        return '{"code":"'+self.code+'", "success":"'+self.success+'", "message":"'+self.message+'", "clearCache":"'+self.clearCache+'"}'
+
+    def toTuple(self):
+        ret = (self.code, self.success, self.message, self.clearCache)
+        return ret
