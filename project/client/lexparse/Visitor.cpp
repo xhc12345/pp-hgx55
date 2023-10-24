b8c1380907e6e7c6b19881c20b48eac2e1bfdbe3
Haochen Xu <hcxu@utexas.edu>
2023-10-24 04:35:30 -0500

    added comments in lexparse for future smart cache system

--- project/client/lexparse/Visitor.cpp
+++ project/client/lexparse/Visitor.cpp
@@ -6,3 +6,11 @@ std::any Visitor::visitOC_Cypher(CypherParser::OC_CypherContext* ctx) {
   std::cout << "Starts visiting..." << std::endl;
   return visitChildren(ctx);
 }
+
+/* 
+  TODO: add functionality to check if graph-modifying keywords are present
+  and store it in a file in IO folder.
+  Thoughts: when visiting terminals like "CREATE" or "MERGE" that are operands,
+  not user-defined IDs, trigger a global flag. Once returned to visitOC_Cypher,
+  generate new file with flag.
+*/
