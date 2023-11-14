ef3c8ad9b207fb5fe31f1184e9b85b08f38a9026
xhc12345 <hcxu@utexas.edu>
2023-11-14 00:14:34 -0600

    runs 1 benchmark 100 times and calculate avg

--- project/benchmark/curl.sh
+++ project/benchmark/curl.sh
@@ -15,12 +15,27 @@ if [ ! -f "$file_name" ]; then
   exit 1
 fi
 
-# Measure the execution time of the curl command
-start_time=$(date +%s.%N)
-curl -s -o /dev/null -X POST -H "Content-Type: application/octet-stream" -d "@$file_name" "$api_endpoint"
-end_time=$(date +%s.%N)
+# Number of iterations
+iterations=100
 
-# Calculate the execution time
-execution_time=$(echo "$end_time - $start_time" | bc)
-# Print the execution time
-echo "Curl command took $execution_time seconds to execute."
+# Initialize total execution time
+total_execution_time=0
+
+# Run the curl command multiple times
+for ((i=1; i<=$iterations; i++)); do
+  start_time=$(date +%s.%N)
+  curl -s -o /dev/null -X POST -H "Content-Type: application/octet-stream" -d "@$file_name" "$api_endpoint"
+  end_time=$(date +%s.%N)
+
+  # Calculate the execution time for each iteration
+  execution_time=$(echo "$end_time - $start_time" | bc)
+
+  # Add to the total execution time
+  total_execution_time=$(echo "$total_execution_time + $execution_time" | bc)
+done
+
+# Calculate the average execution time
+average_execution_time=$(echo "scale=4; $total_execution_time / $iterations" | bc)
+
+# Print the average execution time
+echo "Average execution time over $iterations iterations: $average_execution_time seconds. Total time: $total_execution_time seconds"
