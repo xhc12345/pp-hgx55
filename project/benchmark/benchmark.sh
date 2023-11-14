7cbf35a0d4f6bb146be31cb54f10191941e3ac10
xhc12345 <hcxu@utexas.edu>
2023-11-13 23:59:24 -0600

    benchmark and more route testing

---
+++ project/benchmark/benchmark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dir=$(dirname "$0")
+cd $dir
+
+echo "STARTING BENCHMARK"
+
--- project/server/web-service-gin/routes_test.go
+++ project/server/web-service-gin/routes_test.go
@@ -12,7 +12,7 @@ import (
 func TestPingRoute(t *testing.T) {
 	router := setupRouter()
 	w := httptest.NewRecorder()
-	
+
 	reqPing, _ := http.NewRequest("GET", "/ping", nil)
 	router.ServeHTTP(w, reqPing)
 	assert.Equal(t, 200, w.Code)
@@ -24,8 +24,17 @@ func TestRunRoute(t *testing.T) {
 
 	testRun := []byte("CREATE (n:Person {name: 'Alice', age: 30}) RETURN n" + "CREATE (n:Person {name: 'Bob', age: 28}) RETURN n" + "CREATE (n:Company {name: 'Acme Inc.'}) RETURN n" + "MATCH (person:Person {name: 'Alice'}), (company:Company {name: 'Acme Inc.'}) CREATE (person)-[:WORKS_AT]->(company)" + "MATCH (alice:Person {name: 'Alice'}), (bob:Person {name: 'Bob'}) CREATE (alice)-[:FRIENDS_WITH]->(bob)" + "MATCH (person:Person) RETURN person" + "MATCH (company:Company) RETURN company")
 	testRunReader := bytes.NewReader(testRun)
-	
+
 	reqRun, _ := http.NewRequest("POST", "/run", testRunReader)
+	router.ServeHTTP(w, reqRun)
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestGraphRoute(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+
+	reqRun, _ := http.NewRequest("GET", "/graph", nil)
 	router.ServeHTTP(w, reqRun)
 	assert.Equal(t, 200, w.Code)
 }
